708a691971dc6ed66928a5a5b4561690f6e0e6f1
Steven Aelbrecht <steven.aelbrecht@student.arteveldehs.be>
2021-11-07 19:48:05 +0100

    Added future Islands to io page

---
+++ future-islands/js/data.js
@@ -0,0 +1,223 @@
+/*
+		Topic: Assignment 2 - Digital Agency - Future Island - Data
+		Author: Steven Aelbrecht
+		Modified: 2021-11-03
+*/
+
+const navLinks = [{
+		'name': 'Home',
+		'link': 'https://www.rockwerchter.be/en/',
+		'type': 'internal'
+	},
+	{
+		'name': 'Line-up',
+		'link': 'index.html',
+		'type': 'internal'
+	}, {
+		'name': 'Tickets',
+		'link': 'https://www.rockwerchter.be/en/tickets',
+		'type': 'internal'
+	}, {
+		'name': 'Praktisch',
+		'link': 'https://www.rockwerchter.be/en/',
+		'type': 'internal'
+	}, {
+		'name': 'Camping',
+		'link': 'https://www.rockwerchter.be/en/camping',
+		'type': 'internal'
+	}, {
+		'name': 'History',
+		'link': 'https://www.rockwerchter.be/en/history',
+		'type': 'internal'
+	}, {
+		'name': 'Rwtv',
+		'link': 'https://www.proximus.be/pickx/nl/muziek/festivals/rockwerchter/vod',
+		'type': 'external'
+	},
+	{
+		'name': 'Nmw',
+		'link': 'http://northwestwalls.be/',
+		'type': 'external'
+	},
+	{
+		'name': 'Koop tickets',
+		'link': 'https://www.ticketmaster.be/feature/rockwerchter/?language=en-us&utm_source=rockwerchter.be&utm_medium=website&utm_campaign=rw22&_ga=2.143841778.69334452.1636061307-810472132.1635950746',
+		'type': 'external'
+	},
+];
+
+const dataLineUp = [{ // Faith No More
+		'id': '3e350abd-b825-4f03-87f4-bad3c116744b',
+		'artist': {
+			'name': 'Faith No More',
+			'synopsis': 'Faith No More have always gone against the flow. The Americans do so with humour, a healthy sense of perspective, and a strong faith in their own talent.  This is not just an exceptional band; they are also of great historical importance. In the 1980s, they were the first to blend metal with funk and hip-hop as the founders of what became known as ‘crossover’. Hits like ‘We Care A Lot’ and ‘Epic’ paved the way for Nirvana, while they also inspired Korn and Limp Bizkit. These musical mavericks next shocked friend and foe alike with a smooth cover of ‘Easy’, the Commodores’ soul classic. This in turn made an impression on bands like System Of A Down and Deftones. The watchword is freedom, the outcome is modern metal. Following five years of inactivity, the San Francisco quintet is once again heading out on a world tour.',
+			'social': {
+				'website': 'https://www.fnm.com/',
+				'facebook': 'https://www.facebook.com/faithnomore',
+				'twitter': 'https://www.twitter.com/faithnomore',
+				'instagram': 'https://www.instagram.com/faithnomore'
+			},
+			'picture': {
+				'small': 'https://assets.rockwerchter.be/files/cache/medium/files/fnm-1500-60cc6b19a9efc.jpg',
+				'large': 'https://assets.rockwerchter.be/files/cache/medium/files/fnm-1500-60cc6b19a9efc.jpg'
+			},
+			'media': {
+				'type': 'youtube', // image or youtube
+				'sourceId': 'https://youtu.be/ZG_k5CSYKhg'
+			},
+		},
+		'place': {
+			'name': 'The Barn' // Main Stage, KluB C, The Barn of The Slope
+		},
+		'from': 1656783000000, // 02-07-22 > 19.30
+		'to': 1656786600000, // 02-07-22 > 20.30
+		'createdAt': 1635963000000,
+		'modifiedAt': 1635963000000,
+		'isHeadliner': true
+	},
+	{ // Imagine Dragons
+		'id': '569a144d-b55e-429d-8b61-0d8bef64f011',
+		'artist': {
+			'name': 'Imagine Dragons',
+			'synopsis': 'Imagine Dragons are coming to Rock Werchter next summer! The American rock band announced their Mercury Tour, which takes them to Rock Werchter on Saturday 2 July. Hits like ‘Radioactive’, ‘Believer’ and ‘Thunder’ and their latest album ‘Mercury – Act 1’ proves that Imagine Dragons are among the biggest bands of their generation. With their swaggering songs full of heavy bass and intense drumming, big emotions and the powerful voice of Dan Reynolds, they will set the Festivalpark on fire. Are you ready?',
+			'social': {
+				'website': 'https://www.imaginedragonsmusic.com/',
+				'facebook': 'https://www.facebook.com/imaginedragons',
+				'twitter': 'https://www.twitter.com/imaginedragons',
+				'instagram': 'https://www.instagram.com/imaginedragons'
+			},
+			'picture': {
+				'small': 'https://assets.rockwerchter.be/files/cache/medium/files/id-website2000-617be270c6636.jpg',
+				'large': 'https://assets.rockwerchter.be/files/cache/medium/files/id-website2000-617be270c6636.jpg'
+			},
+			'media': {
+				'type': 'youtube', // image or youtube
+				'sourceId': 'https://youtu.be/Y2NkuFIlLEo'
+			},
+		},
+		'place': {
+			'name': 'Main Stage' // Main Stage, KluB C, The Barn of The Slope
+		},
+		'from': 1656702000000, // 01-07-22 > 21.00
+		'to': 1656707400000, // 01-07-22 > 22.30
+		'createdAt': 1635963000000,
+		'modifiedAt': 1635963000000,
+		'isHeadliner': true
+	},
+	{ // Red Hot Chili Peppers
+		'id': 'c5d60c70-3449-47e5-aa12-ed98a6fd5d23',
+		'artist': {
+			'name': 'Red Hot Chili Peppers',
+			'synopsis': 'Red Hot Chili Peppers are their own musical planet. The Californians introduced their original hybrid of rock and funk in the 1980s. Their danceable, distinctive and deeply energetic sound have provided a soundtrack that several generations have grown up to. The four-piece reached maturity with the album ‘Blood Sugar Sex Magik’ in 1991 – 30 years ago this year. ‘Give It Away’ and ‘Under The Bridge’ brought them their first taste of global fame, but it was ‘Californication’ (1999) – with ‘Scar Tissue’ and ‘Otherside’ - and ‘By the Way’ (2002) that really took the band to the top. The band have more than left their mark every time they’ve graced the Werchter stage, like when bassist Flea played in the nude in 1996. The group will be at its strongest in 2022, with star guitarist John Frusciante back on board.',
+			'social': {
+				'website': 'https://redhotchilipeppers.com/',
+				'facebook': 'https://www.facebook.com/chilipeppers',
+				'twitter': 'https://www.twitter.com/chilipeppers',
+				'instagram': 'https://www.instagram.com/chilipeppers'
+			},
+			'picture': {
+				'small': 'https://assets.rockwerchter.be/files/cache/medium/files/rhcp-web-61543fbab0b59.jpg',
+				'large': 'https://assets.rockwerchter.be/files/cache/medium/files/rhcp-web-61543fbab0b59.jpg'
+			},
+			'media': {
+				'type': 'youtube', // image or youtube
+				'sourceId': 'https://youtu.be/Q0oIoR9mLwc'
+			},
+		},
+		'place': {
+			'name': 'Main Stage' // Main Stage, KluB C, The Barn of The Slope
+		},
+		'from': 1656873000000, // 03-07-22 > 20.30
+		'to': 1656878400000, // 03-07-22 > 22.00
+		'createdAt': 1635963000000,
+		'modifiedAt': 1635963000000,
+		'isHeadliner': true
+	},
+	{ // Twenty One Pilots
+		'id': '80e26b78-8f00-458b-ad84-a6f30186eb3a',
+		'artist': {
+			'name': 'Twenty One Pilots',
+			'synopsis': 'Twenty One Pilots are among the greatest success stories of the 21st century. The numbers don’t lie. Their breakthrough, ‘Blurryface’ (2015), was the first album of the digital era on which all the songs have gone at least gold in the United States. The single ‘Stressed Out’ made the duo the first rock act to achieve more than a billion streams on Spotify. Drummer Josh Dun and keyboard wizard Tyler Joseph are born entertainers. On stage, this means fancy dress, a burning car and backflips off the piano.  Anything goes. Twenty One Pilots appeared at Werchter for the first time in 2014, early in the day in KluB C. They return as an undisputed top act, bringing the recently-released ‘Scaled and Icy’ with them.',
+			'social': {
+				'website': 'https://www.twentyonepilots.com/',
+				'facebook': 'https://www.facebook.com/twentyonepilots',
+				'twitter': 'https://www.twitter.com/twentyonepilots',
+				'instagram': 'https://www.instagram.com/twentyonepilots'
+			},
+			'picture': {
+				'small': 'https://assets.rockwerchter.be/files/cache/medium/files/top-1500-60cc6b1984834.jpg',
+				'large': 'https://assets.rockwerchter.be/files/cache/medium/files/top-1500-60cc6b1984834.jpg'
+			},
+			'media': {
+				'type': 'youtube', // image or youtube
+				'sourceId': 'https://youtu.be/2sBRnnnZyFw'
+			},
+		},
+		'place': {
+			'name': 'The Slope' // Main Stage, KluB C, The Barn of The Slope
+		},
+		'from': 1656784800000, // 02-07-22 > 20.00
+		'to': 1656789300000, // 02-07-22 > 21.15
+		'createdAt': 1635963000000,
+		'modifiedAt': 1635963000000,
+		'isHeadliner': false
+	},
+	{ // Pearl Jam
+		'id': 'ac669686-9415-487b-b3fa-4632f8d5b9d9',
+		'artist': {
+			'name': 'Pearl Jam',
+			'synopsis': 'Pearl Jam’s summer 2021 European tour has officially been rescheduled for June and July of 2022. The new European tour dates for the summer of 2022 are set. And so the first name for Rock Werchter 2022 is in. On Thursday, June 30, 2022, Pearl Jam will headline the opening day of the festival.',
+			'social': {
+				'website': 'https://pearljam.com/',
+				'facebook': 'https://www.facebook.com/pearljam',
+				'twitter': 'https://www.twitter.com/pearljam',
+				'instagram': 'https://www.instagram.com/pearljam'
+			},
+			'picture': {
+				'small': 'https://assets.rockwerchter.be/files/cache/medium/files/14686-pearl-jam-032020-6c9a9964-flat-lowres-5f1a952633f63.jpg',
+				'large': 'https://assets.rockwerchter.be/files/cache/medium/files/14686-pearl-jam-032020-6c9a9964-flat-lowres-5f1a952633f63.jpg'
+			},
+			'media': {
+				'type': 'youtube', // image or youtube
+				'sourceId': 'https://youtu.be/qM0zINtulhM'
+			},
+		},
+		'place': {
+			'name': 'Main Stage' // Main Stage, KluB C, The Barn of The Slope
+		},
+		'from': 1656614700000, // 30-06-22 > 20.45
+		'to': 1656620100000, // 30-06-22 > 22.15
+		'createdAt': 1635963000000,
+		'modifiedAt': 1635963000000,
+		'isHeadliner': true
+	},
+];
+
+const festivalDate = 1656597600000;
+
+const socialIcons = [{
+		'name': 'Facebook',
+		'link': 'http://www.facebook.com/rockwerchterfestival',
+		'type': 'facebook'
+	},
+	{
+		'name': 'Twitter',
+		'link': 'http://www.twitter.com/rockwerchter',
+		'type': 'twitter'
+	},
+	{
+		'name': 'Instagram',
+		'link': 'http://instagram.com/rockwerchterfestival',
+		'type': 'instagram'
+	},
+	{
+		'name': 'Spotify',
+		'link': 'https://play.spotify.com/user/rockwerchterofficial',
+		'type': 'spotify'
+	},
+	{
+		'name': 'Youtube',
+		'link': 'http://www.youtube.com/user/rockwerchterfestival',
+		'type': 'youtube'
+	},
+]
---
+++ future-islands/js/main.js
@@ -0,0 +1,237 @@
+/*
+		Topic: Assignment 2 - Digital Agency - Future Island - Main JS
+		Author: Steven Aelbrecht
+		Modified: 2021-11-05
+*/
+
+(() => {
+	const app = {
+		init() {
+			console.log('1. Init app.')
+			this.cacheElements();
+			this.buildUI();
+		},
+		cacheElements() {
+			console.log('2. Cache all DOM elements.');
+			this.$navElements = document.querySelector('.nav__menu');
+			this.$lineUpElements = document.querySelector('.festival__lineup');
+			this.$concertDetails = document.querySelector('.concert__details');
+			this.$countdownTimer = document.querySelector('.festival__countdown');
+			this.$socialElements = document.querySelector('.soc__menu');
+
+			this.$thursdayElement = document.getElementById('thursday');
+			this.$fridayElement = document.getElementById('friday');
+			this.$saturdayElement = document.getElementById('saturday');
+			this.$sundayElement = document.getElementById('sunday');
+		},
+		buildUI() {
+			console.log('3. Build the UI for the app.');
+			this.$navElements.innerHTML = this.createNavMenu();
+			this.$lineUpElements.innerHTML = this.createLineUp();
+
+			this.openCloseDetails();
+			// this.filterView(); // is not working like I want it to...
+
+			setInterval(() => {
+				this.ticking();
+			}, 500);
+
+			this.$socialElements.innerHTML = this.createSocialMenu();
+		},
+		createNavMenu() {
+			let outputNav = '';
+			outputNav += navLinks.map((nav) => {
+				if (nav.type == 'external') {
+					return `<li class="nav__item nav__item--external"><a href="${nav.link}" target="_blank">${nav.name}</a></li>`;
+				} else {
+					return `<li class="nav__item"><a href="${nav.link}" target="_blank">${nav.name}</a></li>`;
+				};
+			}).join('');
+			return outputNav;
+		},
+		createLineUp() {
+			let outputLineUp = '';
+
+			outputLineUp += dataLineUp.map((artist) => {
+				const dayOfPlay = new Date(artist.from).getDay();
+				const cssBtnOpen = 'open--' + artist.id;
+				const cssBtnClose = 'close--' + artist.id;
+				const cssId = 'concert__details--' + artist.id;
+
+				let lineUpOutput = '';
+
+				lineUpOutput = `<div class="lineup__info filter-day__${this.getDayStr(dayOfPlay)}" id="${cssBtnOpen}" style="display: block;">
+					<img src="${artist.artist.picture.small}" alt="${artist.artist.name}" class="lineup__img">
+					<div class="lineup__text">
+					<p>
+					<span class="lineup__text--day">${this.getDayStr(dayOfPlay)}</span>
+					<span class="lineup__text--place">${artist.place.name}</span>
+					</p>
+					<p class="lineup__text--artist">${artist.artist.name}</p>
+					</div>
+					</div>
+					<div class="concert__details" id="${cssId}" style="display: none;">
+					<img src="images/icons/png/close.png" alt="close" class="btn--close" id="${cssBtnClose}">
+					<img src="${artist.artist.picture.large}" alt="${artist.artist.name}" class="details__img">
+					<div class="details__text">
+					<div class="details__artist">
+					<p>
+					<span class="details__artist--day">${this.getDayStr(dayOfPlay)}</span>
+					<span class="details__artist--place">${artist.place.name}</span>
+					</p>
+					<p class="details__artist--artist">${artist.artist.name}</p>
+					</div>
+					<p class="details__artist--synopsis">${artist.artist.synopsis}</p>
+					<div class="details__artist--synopsis">
+					<iframe src="${artist.artist.media.sourceId}" frameborder="0" allowfullscreen></iframe>
+					</div>
+					<h3 class="">KNOW MORE?</h3>
+					<p><a href="${artist.artist.social.website}">${artist.artist.social.website}</a></p>
+					<p><a href="${artist.artist.social.facebook}">${artist.artist.social.facebook}</a></p>
+					<p><a href="${artist.artist.social.instagram}">${artist.artist.social.instagram}</a></p>
+					<p><a href="${artist.artist.social.twitter}">${artist.artist.social.twitter}</a></p>
+					</div>
+					</div>`
+
+				return lineUpOutput
+			}).join('');
+
+			return outputLineUp;
+		},
+		openCloseDetails() {
+			dataLineUp.map((artist) => {
+				this.$idElement = document.querySelector(`#concert__details--${artist.id}`);
+				this.$btnOpen = document.querySelector(`#open--${artist.id}`);
+				this.$btnClose = document.querySelector(`#close--${artist.id}`);
+				let changeCSS = this.$idElement;
+				let btnOpen = this.$btnOpen;
+				let btnClose = this.$btnClose;
+				btnOpen.addEventListener('click',
+					() => {
+						if (changeCSS.style.display === 'none') {
+							return changeCSS.style.display = 'block';
+						} else {
+							return changeCSS.style.display = 'none';
+						}
+					},
+					false);
+				btnClose.addEventListener('click',
+					() => {
+						if (changeCSS.style.display !== 'none') {
+							return changeCSS.style.display = 'none';
+						} else {
+							return changeCSS.style.display = 'block';
+						}
+					},
+					false);
+			}).join('');
+
+		},
+		filterView() {
+			let filterThursday = document.getElementById(`thursday`);
+			let filterFriday = document.getElementById(`friday`);
+			let filterSaturday = document.getElementById(`saturday`);
+			let filterSunday = document.getElementById(`sunday`);
+
+			dataLineUp.map((day) => {
+				let thursday = document.querySelector(`.filter-day__Thursday`);
+				let friday = document.querySelector(`.filter-day__Friday`);
+				let saturday = document.querySelector(`.filter-day__Saturday`);
+				let sunday = document.querySelector(`.filter-day__Sunday`);
+
+				filterThursday.addEventListener('click',
+					() => {
+						if (thursday.style.display === 'block') {
+							return saturday.style.display = 'none';
+						}
+					},
+					false);
+				filterFriday.addEventListener('click',
+					() => {
+						if (friday.style.display !== 'none') {
+							return friday.style.display = 'none';
+						} else {
+							return friday.style.display = 'block';
+						}
+					},
+					false);
+				filterSaturday.addEventListener('click',
+					() => {
+						if (saturday.style.display !== 'none') {
+							return saturday.style.display = 'none';
+						} else {
+							return saturday.style.display = 'block';
+						}
+					},
+					false);
+				filterSunday.addEventListener('click',
+					() => {
+						if (sunday.style.display !== 'none') {
+							return sunday.style.display = 'none';
+						} else {
+							return sunday.style.display = 'block';
+						}
+					},
+					false);
+			});
+
+		},
+		getDayStr(day) {
+			let output = '';
+			switch (day) {
+				case 0:
+					output = 'Sunday';
+					break;
+				case 1:
+					output = 'Monday';
+					break;
+				case 2:
+					output = 'Tuesday';
+					break;
+				case 3:
+					output = 'Wednesday';
+					break;
+				case 4:
+					output = 'Thursday';
+					break;
+				case 5:
+					output = 'Friday';
+					break;
+				case 6:
+					output = 'Saturday';
+					break;
+			}
+			return output;
+		},
+		createCountdownTimer() {
+			const today = new Date();
+			const diffTime = Math.floor((festivalDate - today) / 1000);
+
+			let mins = Math.floor(diffTime / 60);
+			let secs = diffTime % 60;
+			let hours = Math.floor(mins / 60);
+			mins = mins % 60;
+			let days = Math.floor(hours / 24);
+			hours = hours % 24;
+			days = days % 365;
+
+			let outputStr = `<p>${days} Dagen ${hours}H ${mins}M ${secs}S</p>`;
+			return outputStr;
+		},
+		toAmountOfDigits(number) {
+			let newNumber = '';
+			newNumber = (number < 10) ? newNumber = '0' + number : number;
+			return String(newNumber);
+		},
+		ticking() {
+			this.$countdownTimer.innerHTML = this.createCountdownTimer();
+		},
+		createSocialMenu() {
+			let outputSoc = '';
+			outputSoc += socialIcons.map((link) => `<li class="soc__item"><a href="${link.link}" target="_blank"><img src="images/icons/png/${link.type}.png" alt="${link.name}" class="social__icon"></a></li>`).join('');
+			return outputSoc;
+		}
+	};
+
+	app.init();
+})();
