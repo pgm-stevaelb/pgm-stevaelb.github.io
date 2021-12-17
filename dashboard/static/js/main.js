24448b55922521f033b320064a5c95383a181906
pgm-stevaelb <steven.aelbrecht@student.arteveldehs.be>
2021-12-17 16:04:37 +0100

    Edits push

--- dashboard/static/js/main.js
+++ dashboard/static/js/main.js
@@ -35,7 +35,7 @@
 
 			this.$activeUser = document.getElementById('active-user__details');
 
-			this.$searchBox = document.querySelector('search-box__input');
+			this.$searchBox = document.getElementById('search-box__input');
 			this.$searchButton = document.getElementById('search-box__btn');
 			this.$githubUsers = document.getElementById('github-users__list');
 		},
@@ -44,8 +44,7 @@
 			this.getWeather();
 			this.getCovid();
 			this.getPGMUsers();
-			this.getGithubUsers();
-			this.searchBox();
+			this.getSearchResults();
 		},
 		async getWeather() {
 			const APIURL = 'https://api.weatherapi.com/v1/current.json?key=28a557deaa8c407489e153714211412&q=$Ghent';
@@ -68,11 +67,7 @@
 			}
 		},
 		async getPGMUsers() {
-			await fetch('static/data/pgm.json', {
-					headers: {
-						'Content-Type': 'application/json'
-					}
-				})
+			await fetch('static/data/pgm.json')
 				.then(response => {
 					if (!response.ok) {
 						throw Error('Error, JSON not found.');
@@ -94,24 +89,32 @@
 					this.$listUsers.innerHTML = listUsers;
 				});
 		},
-		async getGithubUsers() {
-			const APIURL = 'https://api.github.com/search/users?sort=desc&page=1&per_page=100&q=pgm-stevaelb';
-			try {
-				const response = await fetch(APIURL);
-				const data = await response.json();
-				this.$githubUsers.innerHTML = `
-				<div class="search-result">
-					<img src="${data.items[0].avatar_url}" alt="${data.items[0].login}" class="search-result__img">
-					<p>${data.items[0].login}</p>
-				</div>`;
-			} catch (error) {
-				console.error(error.message);
-			}
+		getSearchResults() {
+			this.$searchButton.addEventListener('click', () => {
+				const searchInput = this.$searchBox.value;
+				this.showGithubUsers(searchInput);
+			}, false);
 		},
-		searchBox() {
-			const url = new URL(window.location.href);
-			this.$searchButton.addEventListener("click", () => console.log(this.$searchBox.value), false);
-			console.log(url);
+		async showGithubUsers(input) {
+			const APIURL = 'https://api.github.com/search/users?sort=desc&page=1&per_page=50&q=' + input;
+			console.log(APIURL);
+			try {
+				const response = await fetch(APIURL, {
+					headers: {
+						'Authorization': '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'
+					}
+				});
+				const data = response.json();
+				this.$githubUsers = data.items.map((user, index) => {
+					return `
+						<div class="search-result">
+							<img src="${user.items[index].avatar_url}" alt="${user.items[index].login}" class="search-result__img">
+							<p>${user.items[index].login}</p>
+						</div>`
+				}).join('');
+			} catch (e) {
+				console.error('error');
+			}
 		}
 	};
 
