266b43c61a26c06ce80dd7381d9fd3e867532db6
Steven Aelbrecht <aes@broeders.be>
2022-05-24 10:02:29 +0200

    Changes to some HTML and CSS

--- portfolio-new-2/assets/js/main.js
+++ portfolio-new-2/assets/js/main.js
@@ -33,43 +33,6 @@
 			} else {
 				document.getElementById('dark-mode').src = 'assets/media/images/moon.svg';
 			}
-		},
-		mouseMovement() {
-			const cursor = document.querySelector('.cursor');
-			const cursorinner = document.querySelector('.cursor2');
-			const a = document.querySelectorAll('a, img');
-
-			document.addEventListener('mousemove', function(e) {
-				const x = e.clientX;
-				const y = e.clientY;
-				cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-			});
-
-			document.addEventListener('mousemove', function(e) {
-				const x = e.clientX;
-				const y = e.clientY;
-				cursorinner.style.left = x + 'px';
-				cursorinner.style.top = y + 'px';
-			});
-
-			document.addEventListener('mousedown', function() {
-				cursor.classList.add('click');
-				cursorinner.classList.add('cursorinnerhover')
-			});
-
-			document.addEventListener('mouseup', function() {
-				cursor.classList.remove('click')
-				cursorinner.classList.remove('cursorinnerhover')
-			});
-
-			a.forEach(item => {
-				item.addEventListener('mouseover', () => {
-					cursor.classList.add('hover');
-				});
-				item.addEventListener('mouseleave', () => {
-					cursor.classList.remove('hover');
-				});
-			})
 		}
 	}
 
