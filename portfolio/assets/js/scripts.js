ce8d674a99e6430b66a1eec2b6af304ef2679d3b
pgm-stevaelb <steven.aelbrecht@student.arteveldehs.be>
2022-06-06 22:54:59 +0200

    Added favicon and some more changes

--- portfolio/assets/js/scripts.js
+++ portfolio/assets/js/scripts.js
@@ -16,10 +16,10 @@
       }
       if (this.currentMode == 'dark') {
 				document.body.classList.add("dark-mode");
-        document.querySelector('.mode-switch').src = 'assets/media/images/sun.svg';
+        document.querySelector('.mode-switch').src = 'assets/media/images/icons/sun.svg';
 			} else {
 				document.body.classList.add("light-mode");
-        document.querySelector('.mode-switch').src = 'assets/media/images/moon.svg';
+        document.querySelector('.mode-switch').src = 'assets/media/images/icons/moon.svg';
 			}
 			this.$modeSwitch = document.querySelector('.mode-switch');
 		},
@@ -44,11 +44,11 @@
       // Check if the current mode is light or dark and turn them around
       if (currentMode == 'light') {
         document.body.classList.replace('light-mode', 'dark-mode');
-        document.querySelector('.mode-switch').src = 'assets/media/images/sun.svg';
+        document.querySelector('.mode-switch').src = 'assets/media/images/icons/sun.svg';
         localStorage.setItem('mode', 'dark');
       } else {
         document.body.classList.replace('dark-mode', 'light-mode');
-        document.querySelector('.mode-switch').src = 'assets/media/images/moon.svg';
+        document.querySelector('.mode-switch').src = 'assets/media/images/icons/moon.svg';
         localStorage.setItem('mode', 'light');
       }
 		}
